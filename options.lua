ddd5c953d905a2d536613eafc2305b65b1b65076
Marco Vermeulen <vermeulen.mp@gmail.com>
2024-02-07 16:17:38 +0000

    Add some default options

--- options.lua
+++ options.lua
@@ -2,10 +2,10 @@
 return {
   opt = {
     -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
+    relativenumber = false, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+    signcolumn = "yes", -- sets vim.opt.signcolumn to auto, yes or no
     wrap = false, -- sets vim.opt.wrap
   },
   g = {
